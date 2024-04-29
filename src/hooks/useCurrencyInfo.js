5f22f9efe14546e7348893977795ead51e6e1562
neel <neelo2003@gmail.com>
2024-04-29 23:53:53 +0530

    FIx Api url and console statements to return data

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -3,12 +3,17 @@ import { useEffect, useState } from "react"
 function useCurrencyInfo(currency) {
     const [data, setData] = useState({})
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
-        console.log(data);
     }, [currency])
-    console.log(data);
+    
+    useEffect(() => {
+        if (Object.keys(data).length !== 0) {
+            console.log(data);
+        }
+    }, [data]);
+
     return data
 }
 
